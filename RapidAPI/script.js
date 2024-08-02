721cee613ff355b9280d66a7e04d5e96e7a2e649
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-02 16:19:17 +0200

    tried APIs

--- RapidAPI/script.js
+++ RapidAPI/script.js
@@ -1,16 +1,23 @@
-const url = 'https://humor-jokes-and-memes.p.rapidapi.com/jokes/create?topics=pirates&max-length=1000';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '5f9073b66emsh0ac544af98a6b79p1a1470jsne2501bac2377',
-		'x-rapidapi-host': 'humor-jokes-and-memes.p.rapidapi.com'
-	}
-};
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+async function FetchJoke(){
+    const url = 'https://api-football-v1.p.rapidapi.com/v2/odds/league/865927/bookmaker/5?page=2';
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': '5f9073b66emsh0ac544af98a6b79p1a1470jsne2501bac2377',
+            'x-rapidapi-host': 'api-football-v1.p.rapidapi.com'
+        }
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
 }
+
+FetchJoke();
+
+const api_key = AIzaSyAoD_L2TTNta47aLfLsy_VbQbyiO4bghWQ
